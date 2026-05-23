90002a6d617aa49a64effd272767ca1b36d1c6f4
Luiz Angelo <luizangelo1919@gmail.com>
2026-05-23 19:11:04 -0300

    load db corrigido para banco local

--- scripts/etl/load_to_db.py
+++ scripts/etl/load_to_db.py
@@ -2,7 +2,7 @@ import os
 
 import pandas as pd
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 
 load_dotenv()
 
@@ -15,20 +15,25 @@ df_fato_vendas = pd.read_csv('data/processed/fato_vendas.csv')
 
 
 def carregar_banco(engine, nome_banco):
+    with engine.begin() as conn:
+        conn.execute(text('DELETE FROM fato_vendas;'))
+        conn.execute(text('DELETE FROM dim_produto;'))
+        conn.execute(text('DELETE FROM dim_cliente;'))
+
     df_dim_cliente.to_sql(
-        'dim_cliente', engine, if_exists='replace', index=False
+        'dim_cliente', engine, if_exists='append', index=False
     )
 
     df_dim_produto.to_sql(
-        'dim_produto', engine, if_exists='replace', index=False
+        'dim_produto', engine, if_exists='append', index=False
     )
 
     df_fato_vendas.to_sql(
-        'fato_vendas', engine, if_exists='replace', index=False
+        'fato_vendas', engine, if_exists='append', index=False
     )
 
     print(f'✅ Carga concluída no {nome_banco}')
 
 
-#carregar_banco(engine_local, 'Postgres local')
 carregar_banco(engine_supabase, 'Supabase')
+carregar_banco(engine_local, 'Postgres local')
